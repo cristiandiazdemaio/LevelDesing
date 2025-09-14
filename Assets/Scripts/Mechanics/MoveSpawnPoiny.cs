6fea21a6cf940c2f5aa7ff721f3421ef8b10c872
cristiandiazdemaio <cristiandiazdemaio@gmail.com>
2025-09-14 12:42:38 -0300

    add particleSystem

--- Assets/Scripts/Mechanics/MoveSpawnPoiny.cs
+++ Assets/Scripts/Mechanics/MoveSpawnPoiny.cs
@@ -11,12 +11,15 @@ namespace Platformer.Mechanics
         public Vector3 new_position;
         public bool has_been_used = false;
         public GameObject spawn_position;
+        public ParticleSystem fire_particle;
         void OnTriggerEnter2D(Collider2D collider)
         {
             var p = collider.gameObject.GetComponent<PlayerController>();
             if (p != null && has_been_used == false)
             {
                 spawn_position.transform.position = new_position;
+                has_been_used = true;
+                fire_particle.Play();
             }
         }
     }
